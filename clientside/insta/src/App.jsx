6743a4b900f86490f0afaa8ceca9391979b3d7e0
Athilathesi <usmanchusman606@gmail.com>
2024-11-29 17:30:27 +0530

    file added

---
+++ clientside/insta/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Index from './components/Index'
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index/>}/>
+      </Routes>
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
