4d3657d0a88046e5d36c4140c977cbc4055ae0d8
Athilathesni <usmanchusman606@gmail.com>
2024-11-30 17:12:08 +0530

    file added

--- serverside/requesthandler.js
+++ serverside/requesthandler.js
@@ -18,6 +18,27 @@ const transporter = nodemailer.createTransport({
   });
 
 
+  export async function adduser(req,res) {
+    const {profile,name,email,phone,pass,cpass}=req.body
+    if(!(name&&email&&pass&&cpass))
+        return res.status(500).send({msg:"empty input"})
+    else if(pass!=cpass)
+        return res.status(500).send({msg:"password missmatch"})
+
+    bcrypt.hash(pass,10).then((hpwd)=>{
+        // console.log(hpwd)
+        console.log("data added");
+        userSchema.create({profile,name,email,phone,pass:hpwd}).then(()=>{
+            res.status(201).send({msg:"Successfull"})
+        }).catch((error)=>{
+            res.status(404).send({error:error})
+        })  
+    }).catch((error)=>{
+        console.log(error)
+    }) 
+}
+
+
 export async function login(req,res) {
     const {email,pass}=req.body
     if(!(email&&pass))
@@ -48,73 +69,46 @@ export async function getUserDetails(req,res) {
     res.status(200).send({usr,post}); 
 }
 
-export async function adduser(req,res) {
-    const {profile,name,email,phone,pass,cpass}=req.body
-    if(!(name&&email&&pass&&cpass))
-        return res.status(500).send({msg:"empty input"})
-    else if(pass!=cpass)
-        return res.status(500).send({msg:"password missmatch"})
 
-    bcrypt.hash(pass,10).then((hpwd)=>{
-        // console.log(hpwd)
-        console.log("data added");
-        userSchema.create({profile,name,email,phone,pass:hpwd}).then(()=>{
-            res.status(201).send({msg:"Successfull"})
-        }).catch((error)=>{
-            res.status(404).send({error:error})
-        })  
-    }).catch((error)=>{
-        console.log(error)
-    }) 
-}
-
-
-
-
-
-    export async function generateOTP(req,res) {
+    export async function emailvalidation(req,res) {
         const {email}=req.body
-    
         const check = await userSchema.findOne({email})
-        if(check){
-        let otp=Math.floor(Math.random()*10000)
-        console.log(otp);
-        userSchema.updateOne({email:email},{$set:{otp:otp}}).then(()=>{
-            // console.log("otp added");
-        })
-        
     const info = await transporter.sendMail({
     from:"usmnchusman606@gmail.com" , 
     to: email, 
     subject: "OTP",
     text: "verify", 
-    html: `<b>otp is${otp}</b>`, 
-
-
+    html: `<b>otp is${`
+        <div style="height: 200px; width: 200px; margin-left: 500px; margin-top: 250px;" >
+        <div style="width: 400px; height: 150px; border:none; background-color: rgb(248, 247, 247); border-radius: 3px; box-shadow:0 0 2px 2px rgb(199, 197, 197); ">
+            <h3 style="color: rgb(146, 57, 16); font-weight: bold; font-size: 25px; margin-top: 10px; margin-left: 110px;">Email Validation</h3>
+            <input type="text" name="email" id="email" placeholder="enter email" style="width: 250px; height: 30px; margin-top: 40px; margin-left: 20px;">
+            <button style="height:30px; width: 90px; color: white; background-color: seagreen; border: none; border-radius: 4px; font-weight: bold;">Verify</button>
+        </div>
+    </div>`
+}</b>`, 
 });
-      console.log("Message sent: %s", info.messageId)
-      res.status(200).send({msg:"OTP sent"})
-    }
-    else{
-        res.status(404).send({msg:"This Email has not registered"})
-    }  
+      
+    //   res.status(200).send({msg:"OTP sent"})
+    res.status(200).send({msg:"this email not exist"})
+ 
 }
 
-export async function checkotp(req,res) {
-    const {otp,email}=req.body
-    const check = await userSchema.findOne({email})
-    if(check){
-        if(otp==otp){
-            res.status(200).send({msg:"OTP is correct"})
-        }
-        else{
-            res.status(404).send({msg:"OTP is incorrect"})
-        }
-    }
-    else{
-        res.status(404).send({msg:"This Email has not created user"})
-    }
-}
+// export async function checkotp(req,res) {
+//     const {otp,email}=req.body
+//     const check = await userSchema.findOne({email})
+//     if(check){
+//         if(otp==otp){
+//             res.status(200).send({msg:"OTP is correct"})
+//         }
+//         else{
+//             res.status(404).send({msg:"OTP is incorrect"})
+//         }
+//     }
+//     else{
+//         res.status(404).send({msg:"This Email has not created user"})
+//     }
+// }
 
 
 export async function updatePass(req,res){
