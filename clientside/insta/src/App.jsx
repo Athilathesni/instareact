fdd4fac91077745b25fe6d01afe3b7069d92404e
Athilathesni <usmanchusman606@gmail.com>
2024-12-02 13:30:58 +0530

    file added

--- clientside/insta/src/App.jsx
+++ clientside/insta/src/App.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Index from './components/Index'
 import Login from './components/Login'
+import Nav from './components/Nav'
+import Reg from './components/Reg'
 function App() {
 
   return (
     <>
     <BrowserRouter>
+    <Nav/>
       <Routes>
-        <Route path="/" element={<Index/>}/>
+        <Route path="/Index" element={<Index/>}/>
         <Route path="/Login" element={<Login/>}/>
+        <Route path="/Reg" element={<Reg/>}/>
       </Routes>
     </BrowserRouter>
       
